92db56e18b5036b097830e5a2cc778aa411a730a
Vitor Hugo <vitorfioriv@gmail.com>
2025-11-26 21:24:10 -0300

    Commit 4

--- Rental.java
+++ Rental.java
@@ -1,20 +1,43 @@
 package Locadora;
 
 public class Rental {
+    private Movie movie;
+    private int daysRented;
 
-   private Movie _movie;
-   private int _daysRented;
+    public Rental(Movie movie, int daysRented) {
+        this.movie = movie;
+        this.daysRented = daysRented;
+    }
 
-   public Rental(Movie movie, int daysRented) {
-      _movie = movie;
-      _daysRented = daysRented;
-   }
+    public int getDaysRented() {
+        return daysRented;
+    }
 
-   public int getDaysRented() {
-      return _daysRented;
-   }
+    public Movie getMovie() {
+        return movie;
+    }
 
-   public Movie getMovie() {
-      return _movie;
-   }
+    public double getCharge() {
+        double result = 0;
+
+        switch (movie.getPriceCode()) {
+            case Movie.REGULAR:
+                result += 2;
+                if (daysRented > 2)
+                    result += (daysRented - 2) * 1.5;
+                break;
+
+            case Movie.NEW_RELEASE:
+                result += daysRented * 3;
+                break;
+
+            case Movie.CHILDRENS:
+                result += 1.5;
+                if (daysRented > 3)
+                    result += (daysRented - 3) * 1.5;
+                break;
+        }
+
+        return result;
+    }
 }
