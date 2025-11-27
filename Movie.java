1c4d04fe2efe4b8d5a699f07fbe22aaf9eaf3abc
Vitor Hugo <vitorfioriv@gmail.com>
2025-11-26 21:44:39 -0300

    Commit 12

--- Movie.java
+++ Movie.java
@@ -1,23 +1,17 @@
 package Locadora;
 
-class Movie {
+public class Movie {
 
     public static final int CHILDRENS = 2;
     public static final int REGULAR = 0;
     public static final int NEW_RELEASE = 1;
 
     private String _title;
-
-    // AGORA É UM PRICE, NÃO MAIS UM INT
     private Price _price;
 
     public Movie(String name, int priceCode) {
         _title = name;
         setPriceCode(priceCode);
-    }
-
-    public String getTitle() {
-        return _title;
     }
 
     public int getPriceCode() {
@@ -29,47 +23,30 @@ class Movie {
             case REGULAR:
                 _price = new RegularPrice();
                 break;
-
             case CHILDRENS:
                 _price = new ChildrensPrice();
                 break;
-
             case NEW_RELEASE:
                 _price = new NewReleasePrice();
                 break;
-
             default:
                 throw new IllegalArgumentException("Incorrect Price Code");
         }
     }
 
-    public double getCharge(int daysRented) {
-        double result = 0;
-
-        switch (this.getPriceCode()) {
-            case REGULAR:
-                result += 2;
-                if (daysRented > 2)
-                    result += (daysRented - 2) * 1.5;
-                break;
-
-            case NEW_RELEASE:
-                result += daysRented * 3;
-                break;
-
-            case CHILDRENS:
-                result += 1.5;
-                if (daysRented > 3)
-                    result += (daysRented - 3) * 1.5;
-                break;
-        }
-        return result;
+    public String getTitle() {
+        return _title;
     }
 
+    // Passo 1: agora Movie só encaminha
+    public double getCharge(int daysRented) {
+        return _price.getCharge(daysRented);
+    }
+
+    // Passo 2: também só encaminha
     public int getFrequentRenterPoints(int daysRented) {
-        if (this.getPriceCode() == NEW_RELEASE && daysRented > 1)
-            return 2;
-        return 1;
+        return _price.getFrequentRenterPoints(daysRented);
     }
 }
+
 
--- Price.java
+++ Price.java
@@ -1,5 +1,40 @@
 package Locadora;
 
 public abstract class Price {
-   public abstract int getPriceCode();
+
+    public abstract int getPriceCode();
+
+    // Passo 4: método movido de Movie para Price
+    public double getCharge(int daysRented) {
+        double result = 0;
+
+        switch (getPriceCode()) {
+
+            case Movie.REGULAR:
+                result += 2;
+                if (daysRented > 2)
+                    result += (daysRented - 2) * 1.5;
+                break;
+
+            case Movie.NEW_RELEASE:
+                result += daysRented * 3;
+                break;
+
+            case Movie.CHILDRENS:
+                result += 1.5;
+                if (daysRented > 3)
+                    result += (daysRented - 3) * 1.5;
+                break;
+        }
+
+        return result;
+    }
+
+    // Passo 2 encaminhado pro Price também, caso já tenha movido
+    public int getFrequentRenterPoints(int daysRented) {
+        if (getPriceCode() == Movie.NEW_RELEASE && daysRented > 1)
+            return 2;
+        else
+            return 1;
+    }
 }
