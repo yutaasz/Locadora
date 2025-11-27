424f4c3dbd6c42432694af821142c5f157629c50
Vitor Hugo <vitorfioriv@gmail.com>
2025-11-26 21:49:32 -0300

    Commit 15

--- NewReleasePrice.java
+++ NewReleasePrice.java
@@ -2,11 +2,19 @@ package Locadora;
 
 public class NewReleasePrice extends Price {
 
+    @Override
     public int getPriceCode() {
         return Movie.NEW_RELEASE;
     }
 
+    @Override
     public double getCharge(int daysRented) {
         return daysRented * 3;
     }
+
+    @Override
+    public int getFrequentRenterPoints(int daysRented) {
+        return (daysRented > 1) ? 2 : 1;
+    }
 }
+
--- Price.java
+++ Price.java
@@ -2,18 +2,12 @@ package Locadora;
 
 public abstract class Price {
 
-   public abstract int getPriceCode();
-   public abstract double getCharge(int daysRented);
+    public abstract int getPriceCode();
+    public abstract double getCharge(int daysRented);
 
-   // *** Novo método movido do Movie ***
-   public int getFrequentRenterPoints(int daysRented) {
-      // Lógica original:
-      // Se o filme for NEW_RELEASE e alugado por mais de 1 dia → retorna 2
-      // Senão → retorna 1
-      if (getPriceCode() == Movie.NEW_RELEASE && daysRented > 1) {
-         return 2;
-      }
-      return 1;
-   }
+    // Versão genérica: sempre 1 ponto
+    public int getFrequentRenterPoints(int daysRented) {
+        return 1;
+    }
 }
 
